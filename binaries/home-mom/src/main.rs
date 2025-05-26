5a1f97744ae0f13717e95af8dc659b45c9065a85
Amos Wenger <amos@bearcove.eu>
2025-05-26 13:19:06 +0200

    home-mom is up

--- binaries/home-mom/src/main.rs
+++ binaries/home-mom/src/main.rs
@@ -1,7 +1,12 @@
+use std::collections::HashMap;
+
 use camino::Utf8PathBuf;
+use config_types::{Environment, TenantConfig, TenantDomain, TenantInfo, WebConfig};
 use facet::Facet;
 use facet_pretty::FacetPretty;
+use mom_types::MomServeArgs;
 use skelly::{eyre, log};
+use tokio::net::TcpListener;
 
 #[derive(Facet)]
 struct Args {
@@ -14,7 +19,12 @@ struct Args {
     pub tenant_config: Utf8PathBuf,
 }
 
-fn main() -> eyre::Result<()> {
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    real_main().await
+}
+
+async fn real_main() -> eyre::Result<()> {
     let args = std::env::args().skip(1).collect::<Vec<String>>();
     let args_str: Vec<&'static str> = args
         .into_iter()
@@ -25,5 +35,41 @@ fn main() -> eyre::Result<()> {
 
     log::info!("Args: {}", args.pretty());
 
-    Ok(())
+    assert_eq!(
+        Environment::default(),
+        Environment::Production,
+        "mom subcommand is only for production right now"
+    );
+
+    let config = libconfig::load().load_mom_config(&args.mom_config)?;
+    let tenant_config = fs_err::tokio::read_to_string(&args.tenant_config).await?;
+    let tenant_list: Vec<TenantConfig> =
+        facet_json::from_str(&tenant_config).map_err(|e| e.into_owned())?;
+    let tenants: HashMap<TenantDomain, TenantInfo> = tenant_list
+        .into_iter()
+        .map(|tc| {
+            (
+                tc.name.clone(),
+                TenantInfo {
+                    base_dir: config.tenant_data_dir.join(tc.name.as_str()),
+                    tc,
+                },
+            )
+        })
+        .collect();
+
+    let listener = TcpListener::bind("[::]:1118").await?;
+
+    libmom::load()
+        .serve(MomServeArgs {
+            config,
+            web: WebConfig {
+                env: Environment::Production,
+                port: 999, // doesn't matter in prod — it's not used
+            },
+            tenants,
+            listener,
+        })
+        .await
+        .map_err(|err| eyre::eyre!(err.to_string()))
 }
